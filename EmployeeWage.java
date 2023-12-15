0d845f7abdd67057d33b50eca5bf7c9f33295502
vaibhavsraut <vaibhav.raut851@gmail.com>
2023-12-15 18:18:02 +0530

    EmployeeWage.java and SnakeAndLadder.java added

---
+++ EmployeeWage.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOURS = 8;
+        int PART_TIME_HOURS = 4;
+        int WORKING_DAYS_PER_MONTH = 20;
+        int MAX_WORKING_HOURS = 100;
+
+        int totalWage = 0;
+        int totalWorkingHours = 0;
+        int totalWorkingDays = 0;
+
+        while ((totalWorkingHours < MAX_WORKING_HOURS) && (totalWorkingDays<WORKING_DAYS_PER_MONTH)){
+            int attendance = new Random().nextInt(2);
+            int dailyWage=0;
+
+            switch (attendance){
+                case 0:
+                    dailyWage += (PART_TIME_HOURS * WAGE_PER_HOUR);
+                    break;
+
+                case 1:
+                    dailyWage += (FULL_DAY_HOURS * WAGE_PER_HOUR);
+                    break;
+            }
+            totalWage+=dailyWage;
+            totalWorkingHours+= (attendance==1) ? FULL_DAY_HOURS : PART_TIME_HOURS;
+            totalWorkingDays++;
+
+        }
+
+        System.out.println("Your total wage is: " + totalWage);
+        System.out.println("Your total hours are: " + totalWorkingHours);
+        System.out.println("Your total working days are: " + totalWorkingDays);
+
+    }
+}
---
+++ SnakeAndLadder.java
@@ -0,0 +1,113 @@
+import java.util.Random;
+
+public class SnakeAndLadder {
+    private static final int WINNING_POSITION = 100;
+    private static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+        playGameWithSinglePlayer();
+        playGameWithTwoPlayers();
+    }
+
+    private static void playGameWithSinglePlayer() {
+        int playerPosition = START_POSITION;
+        int diceRolls = 0;
+
+        while (playerPosition < WINNING_POSITION) {
+            int diceValue = rollDice();
+            int option = getOption();
+
+            switch (option) {
+                case 0:
+                    break;
+                case 1:
+                    playerPosition += diceValue;
+                    break;
+                case 2:
+                    playerPosition -= diceValue;
+                    break;
+            }
+
+
+            if (playerPosition < START_POSITION) {
+                playerPosition = START_POSITION;
+            }
+
+
+            if (playerPosition > WINNING_POSITION) {
+                playerPosition -= diceValue;
+            }
+
+            diceRolls++;
+            System.out.println("Player Position: " + playerPosition);
+        }
+
+        System.out.println("Player won the game in " + diceRolls + " dice rolls");
+    }
+
+    private static void playGameWithTwoPlayers() {
+        int player1Position = START_POSITION;
+        int player2Position = START_POSITION;
+
+        int player1Turns = 0;
+        int player2Turns = 0;
+
+        while (player1Position < WINNING_POSITION && player2Position < WINNING_POSITION) {
+            player1Position = playTurn(player1Position, "Player 1");
+            player1Turns++;
+
+            if (player1Position >= WINNING_POSITION) {
+                System.out.println("Player 1 won the game in " + player1Turns + " turns");
+                break;
+            }
+
+            player2Position = playTurn(player2Position, "Player 2");
+            player2Turns++;
+
+            if (player2Position >= WINNING_POSITION) {
+                System.out.println("Player 2 won the game in " + player2Turns + " turns");
+                break;
+            }
+        }
+    }
+
+
+    private static int playTurn(int playerPosition, String playerName) {
+        int diceValue = rollDice();
+        int option = getOption();
+
+        switch (option) {
+            case 0:
+                break;
+            case 1:
+                playerPosition += diceValue;
+                break;
+            case 2:
+                playerPosition -= diceValue;
+                break;
+        }
+
+
+        if (playerPosition < START_POSITION) {
+            playerPosition = START_POSITION;
+        }
+
+
+        if (playerPosition > WINNING_POSITION) {
+            playerPosition -= diceValue;
+        }
+
+        System.out.println(playerName + "'s Position: " + playerPosition);
+        return playerPosition;
+    }
+
+    private static int rollDice() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
+
+    private static int getOption() {
+        Random random = new Random();
+        return random.nextInt(3);
+    }
+}
